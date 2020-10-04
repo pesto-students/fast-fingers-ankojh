b96c68e493aae967688731990fef17e5c4fef37c
ankojh <ankitkumarojha2@gmail.com>
2020-10-04 23:01:43 +0530

    Add Line Text Component, debounce util and resizeContext

---
+++ src/components/lineText/LineText.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './LineText.css'
+const LineText = () => {
+  return (
+    <div className="App-line-text">
+      <span className="line"></span>
+      <span className="text">the ultimate typing game</span>
+      <span className="line"></span>
+    </div>
+  );
+};
+
+export default LineText;
